419f6b4f57538c33055c6fe7b60190976c992f0f
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2020-09-02 21:01:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,5 +17,13 @@ cd
 apt-get -y install ruby
 gem install lolcat
 
+# setting banner
+rm /etc/issue.net
+wget -O /etc/issue.net "https://raw.githubusercontent.com/gugun09/premscript/master/issue.net"
+sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
+sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+service ssh restart
+service dropbear restart
+
 cd
 rm -f /root/install.sh
